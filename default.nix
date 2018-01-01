73d3d899ff60e771e90b544c7bcf68d0b583e23d
Andrew Mohrland <noreply@mohr.land>
2017-12-31 21:16:31 -0800

    add standard dependencies

--- default.nix
+++ default.nix
@@ -1,10 +1,18 @@
-{ mkDerivation, base, lens, protolude, stdenv, text }:
+{ mkDerivation, async, base, bytestring, containers, deepseq
+, directory, filepath, lens, mtl, optparse-applicative, process
+, protolude, stdenv, stm, text, transformers, unix
+, unordered-containers, vector
+}:
 mkDerivation {
   pname = "project0";
   version = "0.1.0.0";
   src = ./.;
   isLibrary = false;
   isExecutable = true;
-  executableHaskellDepends = [ base lens protolude text ];
+  executableHaskellDepends = [
+    async base bytestring containers deepseq directory filepath lens
+    mtl optparse-applicative process protolude stm text transformers
+    unix unordered-containers vector
+  ];
   license = stdenv.lib.licenses.bsd3;
 }
