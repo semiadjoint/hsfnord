577f27b24272028ebdf38c401e693815048668da
Andrew Mohrland <noreply@mohr.land>
2018-01-01 12:37:29 -0800

    wip

--- Main.hs
+++ Main.hs
@@ -1,25 +1,85 @@
+{-# options_ghc -W -Werror #-}
 {-# language TemplateHaskell #-}
 {-# language NoImplicitPrelude #-}
-{-# options_ghc -W -Werror #-}
+{-# language DeriveFunctor #-}
 
 module Main where
 
-import Protolude
+import Protolude hiding(Print,readFile,lift)
 import Control.Lens
 
-data Person =
-  Person
-  { _name :: Text
-  , _age  :: Int
-  }
--- Use TemplateHaskell to make lenses.
-makeClassy ''Person
+data Free f a =
+  Done a
+  | More (f (Free f a))
 
-jojo :: Person
-jojo = Person { _name = "Jojo", _age = 28 }
+data Coproduct f g a =
+  This (f a)
+  | That (g a)
+
+instance (Functor f, Functor g) => Functor (Coproduct f g) where
+  fmap f (This x) =
+    This (fmap f x)
+  fmap f (That x) =
+    That (fmap f x)
+
+data ReadFile a =
+  ReadFile Text (Text -> a)
+  deriving Functor
+
+lift ::
+  Functor f =>
+  f a
+  -> Free f a
+lift =
+  More . fmap Done
+
+readFile t k =
+  lift (ReadFile t k)
+
+data Print a =
+  Print Text (() -> a)
+  deriving Functor
+
+
+data ReadFilePrint a =
+  ReadFilePrint (Coproduct ReadFile Print a)
+  deriving Functor
+
+program1 ::
+  (Functor f, AsReadFile f, AsPrint f) =>
+  Free f ()
+program1 = do
+  f <- readFile "/x/.bashrc"
+  print f
+
+class AsIO f where
+  asIO ::
+    f a
+    -> IO a
+
+class AsPrint f where
+  _Print ::
+    Prism' (f a) (Print a)
+
+class AsReadFile f where
+  _ReadFile ::
+    Prism' (f a) (ReadFile a)
+
+
+foldFree ::
+  AsIO f =>
+  Free f a
+  -> IO a
+foldFree (Done a) =
+  pure a
+foldFree (More x) =
+  asIO x >>= foldFree
+
+runProgram =
+  foldFree (program1 :: Free ReadFilePrint ())
 
 main :: IO ()
 main = do
-  putText "Hello "
-  putText $ jojo ^. name
-
+  putText "Starting"
+  runProgram
+  putText "Finished"
