51b75d02dcc43a6cfed9bb53e5bb838aafebf804
Andrew Mohrland <noreply@mohr.land>
2018-01-24 23:04:47 -0800

    config stuff

---
+++ src/Config.hs
@@ -0,0 +1,74 @@
+{-# options_ghc -Wwarn #-}
+{-# language TemplateHaskell #-}
+
+module Config where
+
+import Protolude
+import qualified Prelude
+import Control.Lens
+import Data.Ini.Config
+import Options.Applicative
+import qualified Data.Text as T
+
+data PersonCfg =
+  PersonCfg
+  { _pCfgName :: Text
+  , _pCfgAge  :: Text
+  }
+makeLenses '' PersonCfg
+
+data Cfg =
+  Cfg
+  { _personCfg :: PersonCfg
+  }
+makeLenses '' Cfg
+
+cfgParser :: IniParser Cfg
+cfgParser =
+  Cfg <$>
+  (section "person" $
+    PersonCfg <$> field "name" <*> field "age")
+
+loadCfg ::
+  Text
+  -> IniParser Cfg
+  -> IO (Either Text Cfg)
+loadCfg filename parser =
+  readFile (T.unpack filename) &
+  fmap (\ c -> parseIniFile c parser & first T.pack)
+
+data CliCfg = CliCfg
+  { _cfgFile :: Text
+  }
+
+cliConfig ::
+  Parser CliCfg
+cliConfig =
+  CliCfg . T.pack <$> strOption (long "config-file" <> short 'f' <> help ".ini config file")
+
+parseCli ::
+  Parser CliCfg
+  -> Text
+  -> Text
+  -> IO Text
+parseCli cliCfg desc hdr =
+  fmap _cfgFile (execParser opts)
+  where
+    opts = info (cliCfg <**> helper) (fullDesc <> progDesc (T.unpack desc) <> header (T.unpack hdr))
+
+loadConfigOrDie ::
+  Text
+  -> IO Cfg
+loadConfigOrDie cfgFile = do
+  eCfg <- loadCfg cfgFile cfgParser
+  eCfg & either
+    (die . ("Loading config failed: " <>))
+    pure
+
+parseCliConfigFile ::
+  IO Text
+parseCliConfigFile =
+  parseCli cliConfig desc hdr
+  where
+  desc = "an apt app description"
+  hdr = "app name - app description"
--- src/Main.hs
+++ src/Main.hs
@@ -1,10 +1,23 @@
+{-# options_ghc -Wwarn #-}
+
 module Main where
 
 import Protolude
 import Control.Lens
 import Lib
+import Config
+import System.Log.FastLogger
 
-main :: IO ()
+main ::
+  IO ()
 main = do
-  let pn = person0 ^. name
+  cfg <- loadConfigOrDie =<< parseCliConfigFile
+  withFastLogger (LogStdout 1) (start cfg)
+
+start ::
+  Cfg
+  -> FastLogger
+  -> IO ()
+start cfg log = do
+  let pn = cfg ^. personCfg . pCfgName
   putText $ "Hello " <> pn
